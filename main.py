789ea147738d219759741b7cb7bc93497a477e99
mathew98 <43654709+matanlaviv98@users.noreply.github.com>
2019-12-22 20:47:12 +0200

    progress
    can change path
    set path
    path is updated every time
    ------------------------------
    implement al other things O_O

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from kivy.app import App
 from kivy.lang.builder import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty
-
+import os
 
 class GUI(App):
     def build(self):
@@ -10,14 +10,45 @@ class GUI(App):
 class WindowManager(ScreenManager):
     """ manage transitions between screens and other properties """
     pass
+
+#self.filechooser.files -> list of files the fileschooser display
+#self.filechooser.selection -> list of selected files\dirs
+
+
+
 class MainScreen(Screen):
     def __init__(self, **kwargs):
         # IDs for kv file widgets and needed values
         super(Screen, self).__init__(**kwargs)
-        self.cpath= ObjectProperty(None)
-
-
-
+        self.path_in=ObjectProperty(None)
+        self.filter_in=ObjectProperty(None)
+        self.filechooser=ObjectProperty(None)
+        self.in_selected=ObjectProperty(None)
+    def insert_path(self):
+        path = self.path_in.text
+        if (len(path)==2 and path[-1]==':'):
+            path+="\\"
+        if (os.path.isdir(path)):
+            self.filechooser.path=path
+        else:
+            self.path_in.text="path isn't valid!"
+    def insert_filter(self):
+        if self.filter_in.text=="":
+            self.filechooser.filters=[]
+        else:
+            self.filechooser.filters=[self.filter_in.text]
+    def insert_selected(self):
+        try:
+            l = eval(self.in_selected.text)
+            if type(l)==list():
+                self.filechooser.selection=eval()
+            else:
+                self.in_selected.text="input is invalid!"
+        except:
+            self.in_selected.text="input is invalid!"
+    #def update_selected(self):
+        #updates the selected files textbox.
+        #self.in_selected.text=self.filechooser.selection
 def main():
     pass
 
